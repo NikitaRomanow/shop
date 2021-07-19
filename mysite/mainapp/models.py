cf4e132d3d30ce8129f929e0c478203ce0e3e6ee
NikitaRomanow <wenk95@mail.ru>
2021-07-19 20:14:44 +0500

    Изменение архитектуры

--- mysite/mainapp/models.py
+++ mysite/mainapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 User = get_user_model()
 
@@ -13,6 +15,9 @@ class Category(models.Model):
         return self.name
     
 class Product(models.Model):
+
+    class Meta:
+        abstract = True
 
     category = models.ForeignKey(Category,verbose_name="Категория", on_delete=models.CASCADE)
     title = models.CharField(max_length = 255,verbose_name="Наименование")
@@ -28,8 +33,10 @@ class Product(models.Model):
 class CartProduct(models.Model):
 
     user = models.ForeignKey("Customer", verbose_name="Покупатель", on_delete=models.CASCADE)
-    cart = models.ForeignKey("Cart", verbose_name="Корзина", on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name="Товар", on_delete=models.CASCADE)
+    cart = models.ForeignKey("Cart", verbose_name="Корзина", on_delete=models.CASCADE, related_name = "related_products")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
 
@@ -40,7 +47,7 @@ class CartProduct(models.Model):
 class Cart(models.Model):
 
     owner = models.ForeignKey("Customer", verbose_name="Владелец", on_delete=models.CASCADE)
-    products = models.ManyToManyField(CartProduct, blank=True)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name = "related_cart")
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
 
@@ -52,17 +59,9 @@ class Customer(models.Model):
 
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name="Номер телефона")
-    address = models.ForeignKey(max_length=255, verbose_name="Адрес")
+    address = models.CharField(max_length=255, verbose_name="Адрес")
 
     def __str__(self):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
 
 
-class Specification(models.Model):
-
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name="Имя товара для характеристик")
-
-    def __str__(self):
-        return "Характеристики для товара: {}".format(self.name)
