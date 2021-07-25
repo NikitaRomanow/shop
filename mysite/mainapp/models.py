289f9573db0c99580eb869f392b44389d92b06df
NikitaRomanow <wenk95@mail.ru>
2021-07-25 20:29:45 +0500

    Добавлен функционал по сортировке по предпочтению

--- mysite/mainapp/models.py
+++ mysite/mainapp/models.py
@@ -6,6 +6,30 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
+
+class LatestProductManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get("with_respect_to")
+        products = []
+        ct_models  = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        products, key = lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse = True
+                    )
+        return products
+
+class LatestProducts:
+    objects = LatestProductManager()
+
+
 class Category(models.Model):
 
     name = models.CharField(max_length=255, verbose_name="Имя категории")
@@ -48,7 +72,7 @@ class Smartphone(Product):
     accum_volume = models.CharField(max_length=255, verbose_name="Объем батареи")
     ram = models.CharField(max_length=255, verbose_name="Оперативная память")
     sd = models.BooleanField(default=True)
-    sd_volume_max = models.CharField(max_length=2, verbose_name="Максимальный объем встраиваемой памяти")
+    sd_volume_max = models.CharField(max_length=255, verbose_name="Максимальный объем встраиваемой памяти")
     main_cam_mp = models.CharField(max_length=255, verbose_name="Главная камера")
     frontal_cam_mp = models.CharField(max_length=255, verbose_name="Фронтальная камера")
 
