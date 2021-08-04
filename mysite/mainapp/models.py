5b5d5fd1a6d3f6ad01e6b2834aa28397ce8f2c2d
NikitaRomanow <wenk95@mail.ru>
2021-08-04 22:22:06 +0500

    Product detail view

--- mysite/mainapp/models.py
+++ mysite/mainapp/models.py
@@ -1,13 +1,15 @@
-import sys
-from PIL import Image
+
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from io import BytesIO
-from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.urls import reverse
 
 User = get_user_model()
+
+def get_product_url(obj, viewname):
+    ct_model = obj.__class__.meta.model_name
+    return reverse(viewname, kwargs = {"ct_model": ct_model, "slug": obj.slug})
 
 class MinResolutionErrorException(Exception):
     pass
@@ -66,27 +68,7 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        # image = self.image
-        # img = Image.open(image)
-        # min_height, min_width = self.MIN_RESOLUTION
-        # max_height, max_width = self.MAX_RESOLUTION
-        # if img.height < min_height or img.width < min_width:
-        #     raise MinResolutionErrorException("Разрешение изображения меньше минимального")
-        # if img.height > max_height or img.width > min_width:
-        #     raise MaxResolutionErrorException("Разрешение изображения больше максимального")
-        image = self.image
-        img = Image.open(image)
-        new_img = img.convert("RGB")
-        resized_new_image = new_img.resize((200,200), Image.ANTIALIAS)
-        filestream = BytesIO()
-        resized_new_image.save(filestream, "JPEG", quality = 90)
-        filestream.seek(0)
-        name = "{}.{}".format(*self.image.name.split("."))
-        self.image = InMemoryUploadedFile(
-            filestream, "ImageField", name, "jpeg/image", sys.getsizeof(filestream), None
-        )
-        super().save(*args, **kwargs)
+
 
 
 class Notebook(Product):
@@ -99,6 +81,9 @@ class Notebook(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+    
+    def get_absolute_url(self):
+        return get_product_url(self, "product_detail")
 
 class Smartphone(Product):
     diagonal = models.CharField(max_length=255, verbose_name="Диагональ")
@@ -113,6 +98,9 @@ class Smartphone(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, "product_detail")
 
 
 
